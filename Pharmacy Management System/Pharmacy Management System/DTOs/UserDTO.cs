d120e0c3223ffbe4ceaad5fbd0ccf851b772fc8c
Haitham Albusaidi <Tankasuar@gmail.com>
2026-08-04 09:58:53 +0400

    Completed USer INput DTO

--- Pharmacy Management System/Pharmacy Management System/DTOs/UserDTO.cs
+++ Pharmacy Management System/Pharmacy Management System/DTOs/UserDTO.cs
@@ -1,6 +1,22 @@
-﻿namespace Pharmacy_Management_System.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+namespace Pharmacy_Management_System.DTOs
 {
-    public class UserDTO
+    // DTO for receiving data from the client
+    public class UserInputDto
     {
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(100, MinimumLength = 6)]
+        public string Password { get; set; } = string.Empty;
+
+        public List<string> Roles { get; set; } = new();
     }
+
 }
