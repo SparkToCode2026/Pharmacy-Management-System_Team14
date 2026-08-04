6334b4a9f50649243184f2893ab9e16020743fea
Haitham Albusaidi <Tankasuar@gmail.com>
2026-08-04 10:00:10 +0400

    Set default values for User string properties

--- Pharmacy Management System/Pharmacy Management System/Models/User.cs
+++ Pharmacy Management System/Pharmacy Management System/Models/User.cs
@@ -7,11 +7,11 @@ namespace Pharmacy_Management_System.Models
         [Key]
         public int UserId { get; set; }
         [Required]
-        public string Username { get; set; }
+        public string Username { get; set; } = string.Empty;
         [Required]
-        public string Email { get; set; }
+        public string Email { get; set; } = string.Empty;
         [Required]
-        public string Password { get; set; }
+        public string Password { get; set; }= string.Empty;
         public DateTime createdAt { get; set; } = DateTime.Now;
 
 
