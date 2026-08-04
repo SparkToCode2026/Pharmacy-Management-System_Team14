42679b572b21d1a336fd3a4f5dda611526757750
Mariyam Talal Al-Hosni <s150340@student.squ.edu.om>
2026-08-04 23:32:49 +0400

    added medicine model

--- Pharmacy Management System/Pharmacy Management System/Models/Medicine.cs
+++ Pharmacy Management System/Pharmacy Management System/Models/Medicine.cs
@@ -2,5 +2,39 @@
 {
     public class Medicine
     {
+        [Key]
+        public int MedicineId { get; set; }
+        [Required]
+        public string MedicineName { get; set; }
+        [Required]
+        public string MedicineDescription { get; set; } = string.Empty;
+        [Required]
+        public double MedicinePrice { get; set; }
+
+        // 1: N Relationship with Manafacturer
+        [ForeignKey("Manufacturer")]
+        public int ManufacturerId { get; set; }
+        public Manufacturer Manufacturer { get; set; }
+
+        // 1: N Relationship with OrderItem
+        public List<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+
+        //N: N Relationship with Prescription
+        public List<Prescription> Prescriptions { get; set; } = new List<Prescription();
+
+        // 1: N Relationship with MedicineCategory
+        [ForeignKey("MedicineCategory")]
+        public int MedicineCategoryId { get; set; }
+        public MedicineCategory MedicineCategory { get; set; }
+
+        //1: N Relationship with Supplier
+        [ForeignKey("Supplier")]
+        public int SupplierId { get; set; }
+        public Supplier Supplier { get; set; }
+
+        // 1: N Relationship with stocklevel
+        public List<StockLevel> StockLevels { get; set; } = new List<StockLevel>();
+
+
     }
 }
