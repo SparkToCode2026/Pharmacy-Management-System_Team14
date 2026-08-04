fdfe5c1fe876e0b485ac11be5ad99105f1171736
Haitham Albusaidi <Tankasuar@gmail.com>
2026-08-04 09:51:38 +0400

    Completed Manufacturer Model

--- Pharmacy Management System/Pharmacy Management System/Models/Manufacturer.cs
+++ Pharmacy Management System/Pharmacy Management System/Models/Manufacturer.cs
@@ -1,6 +1,25 @@
-﻿namespace Pharmacy_Management_System.Models
+﻿using System.ComponentModel.DataAnnotations;
+namespace Pharmacy_Management_System.Models
 {
     public class Manufacturer
     {
+        [Key]
+        public int ManufacturerId { get; set; }
+        [Required]
+        public string ManufacturerName { get; set; }
+        [Required]
+        public string LicenseNumber { get; set; }
+        [Required]
+        public string ContactNumber { get; set; }
+        [Required]
+        public string ContactEmail { get; set; }
+
+        // 1:N Relationship with Medicine
+        public List<Medicine> Medicines { get; set; } = new List<Medicine>();
+
     }
 }
+
+
+
+
