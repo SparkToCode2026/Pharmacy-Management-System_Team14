7df1820d6258ec04a7ee371574af05b949df7247
Noora AlHusaini <hlhussaini402@gmail.com>
2026-08-04 17:09:28 +0400

    implemented the atributies of Branch and CustomerProfile entities

--- Pharmacy Management System/Pharmacy Management System/Models/Branch.cs
+++ Pharmacy Management System/Pharmacy Management System/Models/Branch.cs
@@ -2,5 +2,10 @@
 {
     public class Branch
     {
+        public int BranchId { get; set; }
+        public string BranchName { get; set; }
+        public string BranchAddress { get; set; }
+        public string BranchCity { get; set; }
+        public int BranchPhone { get; set; }
     }
 }
--- Pharmacy Management System/Pharmacy Management System/Models/CustomerProfile.cs
+++ Pharmacy Management System/Pharmacy Management System/Models/CustomerProfile.cs
@@ -2,5 +2,10 @@
 {
     public class CustomerProfile
     {
+        public int CustomerId { get; set; }
+        public string CustomerPhone { get; set; }
+        public string CustomerAddress { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
     }
 }
