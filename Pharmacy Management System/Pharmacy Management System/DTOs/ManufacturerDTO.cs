d11c15513bb20504d3607d1def326a4e164efa98
Haitham Albusaidi <Tankasuar@gmail.com>
2026-08-04 10:03:29 +0400

    Completed Manufacturer Input Dto

---
+++ Pharmacy Management System/Pharmacy Management System/DTOs/ManufacturerDTO.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Pharmacy_Management_System.DTOs
+{
+    // DTO for receiving data from the client
+    public class ManufacturerInputDto
+    {
+        [Required]
+        [StringLength(150, MinimumLength = 2)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
+        public string LicenseNumber { get; set; } = string.Empty;
+
+        [Phone]
+        [StringLength(20)]
+        public string ContactPhone { get; set; } = string.Empty;
+
+        [EmailAddress]
+        [StringLength(150)]
+        public string ContactEmail { get; set; } = string.Empty;
+    }
+}
