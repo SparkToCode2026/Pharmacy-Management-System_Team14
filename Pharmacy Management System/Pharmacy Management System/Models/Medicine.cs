98019d8d59275854a9cfff9ae2cce7e01ed9d80c
Abdul Rahman Al - Zeedi <aalzeedi85@gmail.com>
2026-08-05 03:20:06 +0400

    Add Supplier and Prescription models

--- Pharmacy Management System/Pharmacy Management System/Models/Medicine.cs
+++ Pharmacy Management System/Pharmacy Management System/Models/Medicine.cs
@@ -1,42 +1,51 @@
-﻿namespace Pharmacy_Management_System.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Pharmacy_Management_System.Models
 {
     public class Medicine
     {
         [Key]
         public int MedicineId { get; set; }
+
         [Required]
-        public string MedicineName { get; set; }
+        public string MedicineName { get; set; } = string.Empty;
+
         [Required]
         public string MedicineDescription { get; set; } = string.Empty;
+
         [Required]
         public double MedicinePrice { get; set; }
 
-        // 1: N Relationship with Manafacturer
+        // 1: N Relationship with Manufacturer
         [ForeignKey("Manufacturer")]
         public int ManufacturerId { get; set; }
+
         public Manufacturer Manufacturer { get; set; }
+
         public DateTime MedicineProductionDate { get; set; }
+
         public DateTime MedicineExpiryDate { get; set; }
 
         // 1: N Relationship with OrderItem
         public List<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
 
-        //N: N Relationship with Prescription
-        public List<Prescription> Prescriptions { get; set; } = new List<Prescription();
+        // N: N Relationship with Prescription
+        public List<Prescription> Prescriptions { get; set; } = new List<Prescription>();
 
         // 1: N Relationship with MedicineCategory
         [ForeignKey("MedicineCategory")]
         public int MedicineCategoryId { get; set; }
+
         public MedicineCategory MedicineCategory { get; set; }
 
-        //1: N Relationship with Supplier
+        // 1: N Relationship with Supplier
         [ForeignKey("Supplier")]
         public int SupplierId { get; set; }
+
         public Supplier Supplier { get; set; }
 
-        // 1: N Relationship with stocklevel
+        // 1: N Relationship with StockLevel
         public List<StockLevel> StockLevels { get; set; } = new List<StockLevel>();
-
-
     }
 }
--- Pharmacy Management System/Pharmacy Management System/Models/MedicineCategory.cs
+++ Pharmacy Management System/Pharmacy Management System/Models/MedicineCategory.cs
@@ -1,4 +1,5 @@
-﻿namespace Pharmacy_Management_System.Models
+﻿using System.ComponentModel.DataAnnotations;
+namespace Pharmacy_Management_System.Models
 {
     public class MedicineCategory
     {
