df741f013f814d669ae06742de164590010d842b
Tankasuar <tankasuar@gmail.com>
2026-08-05 01:06:49 +0400

    Revert "Payment & StockLevel Models "

--- Pharmacy Management System/Pharmacy Management System/Models/Payment.cs
+++ Pharmacy Management System/Pharmacy Management System/Models/Payment.cs
@@ -2,29 +2,5 @@
 {
     public class Payment
     {
-        public int PaymentId { get; set; }
-        public decimal Amount { get; set; }
-        public DateTime PaymentDate { get; set; }
-        public PaymentMethod PaymentMethod { get; set; }
-        public PaymentStatus PaymentStatus { get; set; }
-
-        public int OrderId { get; set; }
-        public Order Order { get; set; } = null!;
-    }
-
-    public enum PaymentMethod
-    {
-        Cash,
-        CreditCard,
-        DebitCard,
-        Insurance
-    }
-
-    public enum PaymentStatus
-    {
-        Pending,
-        Completed,
-        Failed,
-        Refunded
     }
 }
