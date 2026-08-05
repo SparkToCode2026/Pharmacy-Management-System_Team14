ac96454b0553292ff7b1f8c084354ad5d76848e2
Haitham Albusaidi <Tankasuar@gmail.com>
2026-08-05 16:56:28 +0400

    Add DbSet for Order and OrderItem to ProjectContext

--- Pharmacy Management System/Pharmacy Management System/ProjectContext.cs
+++ Pharmacy Management System/Pharmacy Management System/ProjectContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Pharmacy_Management_System.Models;
+using PharmacyManagementSystem.Models;
 
 namespace Pharmacy_Management_System
 {
@@ -9,5 +10,8 @@ namespace Pharmacy_Management_System
         public DbSet<User> User { get; set; }
         public DbSet<Medicine> Medicines { get; set; }
         public DbSet<MedicineCategory> MedicineCategories { get; set; }
+        public DbSet<Order> Order { get; set; }
+        public DbSet<OrderItem> OrderItem { get; set; }
+
     }
 }
