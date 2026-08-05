bb8e5e22d65224ff8e973ae3510d0e0b1cea1f54
Haitham Albusaidi <Tankasuar@gmail.com>
2026-08-05 15:05:21 +0400

    Completed UpdateUser

--- Pharmacy Management System/Pharmacy Management System/Controllers/UserController.cs
+++ Pharmacy Management System/Pharmacy Management System/Controllers/UserController.cs
@@ -11,8 +11,12 @@ namespace Pharmacy_Management_System.Controllers
         {
             _context = context;
         }
+
+
+        [HttpPost("register")]
         public IActionResult Register(User U)
         {
+            //Check if the username or email already exists in the database
             bool userExists = _context.User.Any(u => u.Username == U.Username || u.Email == U.Email);
             if (userExists)
             {
@@ -21,6 +25,21 @@ namespace Pharmacy_Management_System.Controllers
             _context.User.Add(U);
             _context.SaveChanges();
             return Ok(U.UserId);
+        }
+        [HttpPut("{id}")]
+        public IActionResult UpdateUser(int id, User U)
+        {
+            //Check if the user exists in the database
+            var user = _context.User.Find(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            user.Username = U.Username;
+            user.Email = U.Email;
+            
+            _context.SaveChanges();
+            return Ok(user);
         }
 
     }
