2b12ad98cfb35dfe6b5e5138aa8eadae47263b7a
Haitham Albusaidi <Tankasuar@gmail.com>
2026-08-05 16:12:10 +0400

    Setup Manufacturer Controller

--- Pharmacy Management System/Pharmacy Management System/Controllers/ManufacturerController.cs
+++ Pharmacy Management System/Pharmacy Management System/Controllers/ManufacturerController.cs
@@ -1,6 +1,15 @@
-﻿namespace Pharmacy_Management_System.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Pharmacy_Management_System.Controllers
 {
-    public class ManufacturerController
+    [ApiController]
+    [Route("[controller]")]
+    public class ManufacturerController : ControllerBase
     {
+        private ProjectContext _context;
+        public ManufacturerController(ProjectContext context)
+        {
+            _context = context;
+        }
     }
 }
