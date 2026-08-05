b3a61dba01ceca8435e29f4e72a29019a568e67f
Mohammed Al-Mahrooqi <mohammed010607@gmail.com>
2026-08-05 20:52:25 +0400

    Committing Create Stock LevelEndpoint

--- Pharmacy Management System/Pharmacy Management System/Models/StockLevel.cs
+++ Pharmacy Management System/Pharmacy Management System/Models/StockLevel.cs
@@ -2,5 +2,23 @@
 {
     public class StockLevel
     {
-    }
+        [ApiController]
+        [Route("StockLevel")]
+        public class StockLevelController : ControllerBase
+        {
+            private ProjectContext _context;
+            public StockLevelController(ProjectContext context)
+            {
+                _context = context;
+            }
+
+            //Create a new stock level entry
+            [HttpPost("CreateStockLevel")]
+            public IActionResult CreateStockLevel([FromBody] StockLevel stockLevel)
+            {
+                _context.StockLevel.Add(stockLevel);
+                _context.SaveChanges();
+                return Ok(stockLevel);
+            }
+        }
 }
