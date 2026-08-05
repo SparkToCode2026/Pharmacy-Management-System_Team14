a990e660aa19b819d38f98e20608aa5164a3ca44
Haitham Albusaidi <Tankasuar@gmail.com>
2026-08-05 15:00:19 +0400

    Completed User Register

--- Pharmacy Management System/Pharmacy Management System/Controllers/UserController.cs
+++ Pharmacy Management System/Pharmacy Management System/Controllers/UserController.cs
@@ -11,7 +11,17 @@ namespace Pharmacy_Management_System.Controllers
         {
             _context = context;
         }
-
+        public IActionResult Register(User U)
+        {
+            bool userExists = _context.User.Any(u => u.Username == U.Username || u.Email == U.Email);
+            if (userExists)
+            {
+                return BadRequest("Username or Email is already taken.");
+            }
+            _context.User.Add(U);
+            _context.SaveChanges();
+            return Ok(U.UserId);
+        }
 
     }
 }
