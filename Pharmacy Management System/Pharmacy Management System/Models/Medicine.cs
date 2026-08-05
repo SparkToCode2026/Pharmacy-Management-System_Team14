765e2d8ca4d47941ae824a0f0df1ba1835e6d00f
Mariyam Talal Al-Hosni <s150340@student.squ.edu.om>
2026-08-05 23:47:19 +0400

    added mediciine and medicine category controlleres + added jsonignore to any navigation proprty in the models

--- Pharmacy Management System/Pharmacy Management System/Models/Medicine.cs
+++ Pharmacy Management System/Pharmacy Management System/Models/Medicine.cs
@@ -1,11 +1,13 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 namespace Pharmacy_Management_System.Models
 {
     public class Medicine
     {
         // Primary Key
         [Key]
+        [JsonIgnore]
         public int MedicineId { get; set; }
 
 
@@ -21,6 +23,7 @@ namespace Pharmacy_Management_System.Models
         // 1:N Relationship with Manufacturer
         [ForeignKey("Manufacturer")]
         public int ManufacturerId { get; set; }
+        [JsonIgnore]
         public Manufacturer Manufacturer { get; set; }
 
         // Medicine Production and Expiry Dates
@@ -29,24 +32,29 @@ namespace Pharmacy_Management_System.Models
 
 
         // 1:N Relationship with OrderItem
+        [JsonIgnore]
         public List<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
 
 
         // N:N Relationship with Prescription
+        [JsonIgnore]
         public List<Prescription> Prescriptions { get; set; } = new List<Prescription>();
 
 
         // 1:N Relationship with MedicineCategory
         [ForeignKey("MedicineCategory")]
         public int MedicineCategoryId { get; set; }
+        [JsonIgnore]
         public MedicineCategory MedicineCategory { get; set; }
 
         //1: N Relationship with Supplier
         [ForeignKey("Supplier")]
         public int SupplierId { get; set; }
+        [JsonIgnore]
         public Supplier Supplier { get; set; }
 
         // 1: N Relationship with stocklevel
+        [JsonIgnore]
         public List<StockLevel> StockLevels { get; set; } = new List<StockLevel>();
 
 
