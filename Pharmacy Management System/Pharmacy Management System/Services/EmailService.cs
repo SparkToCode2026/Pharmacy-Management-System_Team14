182a54b8bdd986e0cec0d7cb3a6ca84b2ec77b12
Mariyam Talal Al-Hosni <s150340@student.squ.edu.om>
2026-08-15 12:34:56 +0400

    Added a service folder + 1 enterface and 1 email class that uses said interface

---
+++ Pharmacy Management System/Pharmacy Management System/Services/EmailService.cs
@@ -0,0 +1,6 @@
+﻿namespace Pharmacy_Management_System.Services
+{
+    public class EmailService
+    {
+    }
+}
---
+++ Pharmacy Management System/Pharmacy Management System/Services/IEmailService.cs
@@ -0,0 +1,6 @@
+﻿namespace Pharmacy_Management_System.Services
+{
+    public interface IEmailService
+    {
+    }
+}
